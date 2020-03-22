dd2f53c4be3b586da79329ef4d25cdfcac196f52
Vincenzo Santopietro <vincenzo.santopietro@fcagroup.com>
2020-03-22 17:41:54 +0100

    Cleaned code

--- src/main/scala/com/vinx/covid/statistics/metrics/utils/InfluxDBSink.scala
+++ src/main/scala/com/vinx/covid/statistics/metrics/utils/InfluxDBSink.scala
@@ -24,31 +24,6 @@ class InfluxDBSink(dbUrl:String, dbName: String) extends MetricsWriter {
   implicit val awaitAtMost = 10.seconds
   val spark = SparkSession.builder.getOrCreate
 
-  import spark.implicits._
-  private def createDbClient(): Unit = {
-    syncInfluxDb(new URI(dbUrl), "covid-italy") { db =>
-
-      /*val point = Point(
-        time = DateTime.now(),
-        measurement = "measurement",
-        tags = Map("t1" -> "A", "t2" -> "B"),
-        fields = Map(
-          "f1" -> 10.3, // BigDecimal field
-          "f2" -> "x",  // String field
-          "f3" -> -1,   // Long field
-          "f4" -> true) // Boolean field
-      )
-      db.write(point)*/
-
-
-      // Synchronously read the written point
-      //val queryResult = db.query("SELECT * FROM measurement1")
-
-      // Print the single point to the console
-      //println(queryResult.row.mkString)
-    }
-  }
-
   def createFields(row: Row, schema: Array[String]) = {
     var field = Map[FieldKey,FieldValue]()
 
@@ -60,7 +35,6 @@ class InfluxDBSink(dbUrl:String, dbName: String) extends MetricsWriter {
     }
     field
   }
-
 
   override def storeMetrics(metrics: DataFrame): Unit = {
     // List of column names
